9053789e9a40d1bb427e2ad88aa9d9c842d94da5
Marcin Kosecki <marcin.kosecki@int.pl>
2023-03-24 17:16:43 +0100

    Add method navigationTxt to make  hove r effect

--- script/js.js
+++ script/js.js
@@ -20,22 +20,33 @@ const backdrop = document.querySelector(".backdrop");
 const closeIcon = document.querySelector(".fa-xmark");
 const navList = document.querySelector(".navigation__list");
 const carouselWrapper = document.querySelector(".carousel__wrapper");
-const elements = [carouselSecond, backdrop, navList];
+const counterWrapper = document.querySelector('.counter__wrapper')
+const elements = [carouselSecond, backdrop, navList, counterWrapper];
 
 const removeActive = (elements) => {
+  trolley.addEventListener("click", shoWHideBasket);
   elements.forEach((element) => element.classList.remove("active"));
 };
 const addActive = (elements) => {
+  console.log('ok');
+  trolley.removeEventListener("click", shoWHideBasket);
   elements.forEach((element) => element.classList.add("active"));
 };
 
 closeIcon.addEventListener("click", () => removeActive(elements));
 carouselWrapper.addEventListener("click", () => addActive(elements));
 
+//basket
+
+const trolley = document.querySelector(".cart__wrapper");
+const basket = document.querySelector(".basket");
+const shoWHideBasket = () => basket.classList.toggle("active");
+trolley.addEventListener("click", shoWHideBasket);
+
 //second carousel
 const mainButtons = document.querySelectorAll(".indicators__button--main");
 const secondButtons = document.querySelectorAll(".indicators__button--second");
-const carouselSecondItems = document.querySelectorAll('.carousel-item--second')
+const carouselSecondItems = document.querySelectorAll(".carousel-item--second");
 mainButtons.forEach((button) => button.addEventListener("click", setBtn));
 
 function setBtn() {
@@ -47,13 +58,21 @@ function setBtn() {
 }
 
 function addClass(element) {
-  removeActive(carouselSecondItems)
-  removeActive(secondButtons)
-   setTimeout(() => {
-     element.classList.add("active");
-   }, 300); 
+  removeActive(secondButtons);
+  removeActive(carouselSecondItems);
+  setTimeout(() => {
+    element.classList.add("active");
+  }, 300);
 }
 
+
+//navigation hover effect
+
+const navigationTxt = document.querySelectorAll('.navigation__text');
+navigationTxt.forEach(el => {
+  el.addEventListener('mouseover', () => el.parentNode.classList.add('active'))
+  el.addEventListener('mouseout', () => el.parentNode.classList.remove('active'))
+})
 
 
 
